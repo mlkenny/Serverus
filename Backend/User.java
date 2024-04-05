8833af87b1945fb53f3c5eac4c8276f938c7dfd2
mlkenny <MichaelK1734@gmail.com>
2024-04-05 10:38:13 -0700

    Added very rough user class structure. Subject to change.

--- Backend/User.java
+++ Backend/User.java
@@ -1,4 +1,35 @@
 package Backend;
 public class User {
-    
+    int userID;
+    boolean LoggedIn;
+    String email;
+    String password;
+
+    /*<- REMOVE THIS WHEN READY TO IMPLEMENT
+    // searchEmployment Method: Should return the possible employment opportunities based off the given keywords. 
+    //                          Keywords should be given through user input in a text box. (js, html interaction)
+
+    // searchEmployement and searchResources return type may be subject to change to different object if needed.
+    public String[] searchEmployment(String[] keywords) {
+
+    }
+
+    // searchResources Method: Return keyword search matches based on given user input in a text box.
+    //                         This method should be capable of searching through all kinds of resources.
+    //                         eg. Healthcare, Shelters, Food Banks.
+    public String[] searchResources(String[] keywords) {
+
+    }
+
+    // saveLocation Method: Allow the user to save specific locations based on if the user selects the star icon.
+    //                      Saved locations should be stored in the database under the user's ID.
+    public void saveLocations() {
+
+    }
+
+    // submitFeedback Method: Is this necessary at this point with a dedicated feedback page?
+    public void submitFeedback() {
+
+    }
+    REMOVE THIS WHEN READY TO IMPLEMENT ->*/
 }
