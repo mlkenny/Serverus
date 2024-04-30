734147b4d86fa214b326bf14b579c8dca2c70d98
tonedoan <tonytdoan94@gmail.com>
2024-04-29 23:11:02 -0700

    Added map to housing page. Still need to figure out how to fill out parent container.

--- Frontend/GoogleMapsAPI/function.js
+++ Frontend/GoogleMapsAPI/function.js
@@ -75,7 +75,7 @@ async function createMarker(places) {
 
 
 const apiUrl = `https://places.googleapis.com/v1/places:searchText`;
-const apiKey = 'INPUT API HERE'; // Input your API key here.
+const apiKey = 'AIzaSyDPwlEdi4wkJa1ezTXBMyhM391JZzNdxDg'; // Input your API key here.
 
 document.getElementById('location-form').addEventListener('submit', function(event) {
     event.preventDefault();
